abfc1ddf94989640308a2cb432afd1114b65de51
Ahsan Javed <ahsannjavaid@gmail.com>
2025-09-04 22:30:32 +0500

    feat(.env): setup API_URL

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -19,13 +19,15 @@ import {
 } from './types';
 import { setAlert } from './alert';
 
+const API_URL = `${process.env.REACT_APP_API_URL}/api/v1/auth`;
+
 export const loadUser = () => async (dispatch) => {
   if (localStorage.token) {
     axios.defaults.headers.common['Authorization'] = localStorage.token;
   }
 
   try {
-    const res = await axios.get('/api/auth');
+    const res = await axios.get(API_URL);
     dispatch({
       type: USER_LOADED,
       payload: res.data,
@@ -53,7 +55,7 @@ export const register =
       full_name: formData.full_name 
     });
     try {
-      const res = await axios.post(`/api/signup`, body, config);
+      const res = await axios.post(`${API_URL}/signup`, body, config);
       dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data,
@@ -79,7 +81,7 @@ export const login = (email, password) => async (dispatch) => {
   };
   const body = JSON.stringify({ email, password });
   try {
-    const res = await axios.post(`/api/login`, body, config);
+    const res = await axios.post(`${API_URL}/login`, body, config);
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data,
@@ -106,7 +108,7 @@ export const forgotPassword = (email) => async (dispatch) => {
   const body = JSON.stringify({ email });
   try {
     const res = await axios.post(
-      `${API_URL}/api/auth/forgotPassword`,
+      `${API_URL}/forgotPassword`,
       body,
       config
     );
